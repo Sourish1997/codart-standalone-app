393c5b45ea2a5fc6d9d45fea3172ec59ff8e9f3e
sourish1997 <sourish.banerjeee@gmail.com>
2017-02-08 01:01:30 +0530

    Login module functionality implemented.

--- src/body/DrawerContentController.java
+++ src/body/DrawerContentController.java
@@ -1,11 +1,14 @@
 package body;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.sun.org.apache.xml.internal.security.Init;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -23,10 +26,48 @@ public class DrawerContentController implements Initializable{
     private ImageView profilePic;
 
     @FXML
+    private Label drawerUsername;
+
+    @FXML
+    private JFXButton logoutButton;
+
+    @FXML
+    private Label scoreLabel;
+
+    @FXML
     private JFXListView<?> solvedList;
 
     @FXML
+    private Label solvedLabel;
+
+    @FXML
+    private Label solvedEasy;
+
+    @FXML
+    private Label solvedMedium;
+
+    @FXML
+    private Label solvedHard;
+
+    @FXML
     private JFXListView<?> forfeitedList;
+
+    @FXML
+    private Label forfeitedLabel;
+
+    @FXML
+    private Label forfeitedEasy;
+
+    @FXML
+    private Label forfeitedMedium;
+
+    @FXML
+    private Label forfeitedHard;
+
+    @FXML
+    void logoutClicked(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,5 +80,17 @@ public class DrawerContentController implements Initializable{
             e.printStackTrace();
         }
     }
-}
 
+    public void initVariables(String username, int[] userData) {
+        drawerUsername.setText(username.substring(0, 1).toUpperCase() + username.substring(1));
+        scoreLabel.setText("Total Score: " + userData[0]);
+        solvedLabel.setText("Questions Solved: " + userData[1]);
+        solvedEasy.setText("Easy: " + userData[2]);
+        solvedMedium.setText("Medium: " + userData[3]);
+        solvedHard.setText("Hard: " + userData[4]);
+        forfeitedLabel.setText("Questions Forfeited: " + userData[5]);
+        forfeitedEasy.setText("Easy: " + userData[6]);
+        forfeitedMedium.setText("Medium: " + userData[7]);
+        forfeitedHard.setText("Hard: " + userData[8]);
+    }
+}
