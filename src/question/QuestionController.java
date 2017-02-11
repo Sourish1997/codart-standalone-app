9571bf31a50ee8844279d268cd8adeb719a4e59d
sourish1997 <sourish.banerjeee@gmail.com>
2017-02-12 02:27:42 +0530

    Changed logic of transaction validation.

--- src/question/QuestionController.java
+++ src/question/QuestionController.java
@@ -48,7 +48,7 @@ public class QuestionController {
     private Thread timerThread;
     private String difficulty;
     private boolean exit = false;
-    private int index, no;
+    private int index, qNo;
 
     @FXML
     void onBackClick(ActionEvent event) throws Exception {
@@ -67,9 +67,9 @@ public class QuestionController {
         noButton.getStylesheets().add("/main/mainStyle.css");
         yesButton.getStyleClass().add("rect-button-raised");
         noButton.getStyleClass().add("red-rect-button-raised");
+
         HBox hbox = new HBox(10);
         hbox.getChildren().addAll(yesButton, noButton);
-
         content.setActions(hbox);
 
         JFXDialog  dialog = new JFXDialog(mainStack, content, JFXDialog.DialogTransition.CENTER);
@@ -77,46 +77,7 @@ public class QuestionController {
         dialog.show();
 
         yesButton.setOnAction(e -> {
-            int neg;
-            if(difficulty.equals("e"))
-                neg = 20;
-            else if(difficulty.equals("m"))
-                neg = 15;
-            else
-                neg = 10;
-
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader("data/" + username + ".codart"));
-                String username = reader.readLine();
-                int[] userScoreInfo = new int[9];
-                for (int i = 0; i < 9; i++)
-                    userScoreInfo[i] = Integer.parseInt(reader.readLine());
-                reader.close();
-
-                userScoreInfo[5] += 1;
-                if (difficulty.equals("e")) {
-                    userScoreInfo[0] -= 20;
-                    userScoreInfo[6] += 1;
-                } else if (difficulty.equals("m")) {
-                    userScoreInfo[0] -= 15;
-                    userScoreInfo[7] += 1;
-                } else {
-                    userScoreInfo[0] -= 10;
-                    userScoreInfo[8] += 1;
-                }
-
-                writeToUserFile(username, userScoreInfo);
-                writeToUserQuestionData();
-
-                String questions[] = readQuestionsFile();
-                String questionData[] = questions[index].split(" ");
-                questionData[2] = "F";
-                questions[index] = String.join(" ", questionData);
-                writeToQuestionFile(username, questions);
-                dialog.close();
-
-                addNewQuestion();
-            } catch (Exception ex) {}
+            addNewQuestion(1);
         });
 
         noButton.setOnAction(e -> {
@@ -124,12 +85,16 @@ public class QuestionController {
         });
     }
 
-    public void addNewQuestion() {
-        JFXDialogLayout content2 = new JFXDialogLayout();
-        content2.setHeading(new Label(""));
+    @FXML
+    void onQuestionSolved(ActionEvent event) throws Exception {
+        addNewQuestion(2);
+    }
+
+    public void addNewQuestion(int callId) {
         JFXButton okButton = new JFXButton("Ok");
         okButton.getStylesheets().add("/main/mainStyle.css");
         okButton.getStyleClass().add("rect-button-raised");
+
         final ToggleGroup group = new ToggleGroup();
 
         JFXRadioButton easy = new JFXRadioButton("Easy");
@@ -151,99 +116,81 @@ public class QuestionController {
         vbox.getChildren().add(hard);
         vbox.setSpacing(10);
 
-        content2.setBody(vbox);
-        content2.setActions(okButton);
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(new Label(""));
+        content.setBody(vbox);
+        content.setActions(okButton);
 
-        JFXDialog  dialog2 = new JFXDialog(mainStack, content2, JFXDialog.DialogTransition.CENTER);
+        JFXDialog  dialog = new JFXDialog(mainStack, content, JFXDialog.DialogTransition.CENTER);
 
         okButton.setOnAction(e2 -> {
             try {
                 String selected = group.getSelectedToggle().toString();
-                dialog2.close();
                 selected = selected.charAt(selected.length() - 2) + "";
-                System.out.println(selected);
+
                 BufferedReader reader = new BufferedReader(new FileReader("data/noq.codart"));
                 int qNos[] = new int[3];
                 qNos[0] = Integer.parseInt(reader.readLine());
                 qNos[1] = Integer.parseInt(reader.readLine());
                 qNos[2] = Integer.parseInt(reader.readLine());
                 reader.close();
+
                 reader = new BufferedReader(new FileReader("data/" + username + "_qdata.codart"));
                 String solvedState[][] = new String[3][];
                 solvedState[0] = reader.readLine().split(" ");
                 solvedState[1] = reader.readLine().split(" ");
                 solvedState[2] = reader.readLine().split(" ");
                 reader.close();
-                String solveContainer[];
-                int solveNo;
+
+                String newQuestionContainer[];
+                int totalChosenQTypeNumber;
                 if (selected.equals("y")) {
-                    solveContainer = solvedState[0];
-                    solveNo = qNos[0];
+                    newQuestionContainer = solvedState[0];
+                    totalChosenQTypeNumber = qNos[0];
                     selected = "e";
                 } else if (selected.equals("m")) {
-                    solveContainer = solvedState[1];
-                    solveNo = qNos[1];
+                    newQuestionContainer = solvedState[1];
+                    totalChosenQTypeNumber = qNos[1];
                     selected = "m";
                 } else {
-                    solveContainer = solvedState[2];
-                    solveNo = qNos[2];
+                    newQuestionContainer = solvedState[2];
+                    totalChosenQTypeNumber = qNos[2];
                     selected = "h";
                 }
-                int no = (int) Math.random() % solveNo;
+
+                int no = ((int) Math.random()) % totalChosenQTypeNumber;
                 int found = -1;
-                for(int i = no; i < solveContainer.length; i++) {
-                    if(solveContainer[i].equals("F")) {
+                for (int i = no; i < newQuestionContainer.length; i++) {
+                    if (newQuestionContainer[i].equals("F")) {
                         found = i;
                         break;
                     }
                 }
 
-                if(found == -1) {
-                    for(int i = 0; i < no; i++){
-                        if(solveContainer[i].equals("F")) {
+                if (found == -1) {
+                    for (int i = 0; i < no; i++) {
+                        if (newQuestionContainer[i].equals("F")) {
                             found = i;
                             break;
                         }
                     }
                 }
 
-                String newQuestionData = selected + " " + (found + 1) + " NS " + System.nanoTime();
-                BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_questions.codart", true));
-                writer.write(newQuestionData);
-                writer.newLine();
-                writer.close();
-                loadTabs();
+                if(found == -1) {
+                    //TODO: display snackbar here
+                    dialog.close();
+                } else {
+                    dialog.close();
+                    confirmTransaction(selected, found, callId);
+                }
             } catch (Exception e) {}
         });
 
-        dialog2.setOverlayClose(false);
-        dialog2.show();
+        dialog.setOverlayClose(false);
+        dialog.show();
     }
 
-    public void writeToUserQuestionData() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("data/" + username + "_qdata.codart"));
-        String solvedState[][] = new String[3][];
-        solvedState[0] = reader.readLine().split(" ");
-        solvedState[1] = reader.readLine().split(" ");
-        solvedState[2] = reader.readLine().split(" ");
-        reader.close();
-        if(difficulty.equals("e")) {
-            solvedState[0][no - 1] = "T";
-        } else if(difficulty.equals(("m"))) {
-            solvedState[1][no - 1] = "T";
-        } else {
-            solvedState[2][no - 1] = "T";
-        }
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_qdata.codart"));
-        for(int i = 0; i < 3; i++) {
-            writer.write(String.join(" ", solvedState[i]));
-            writer.newLine();
-        }
-        writer.close();
-    }
-
-    @FXML
-    void onSolvedClick(ActionEvent event) throws Exception{
+    public void confirmTransaction(String selected, int found, int callId) {
         JFXDialogLayout content = new JFXDialogLayout();
         content.setHeading(new Label("Enter Verification: "));
         JFXPasswordField pField = new JFXPasswordField();
@@ -254,43 +201,73 @@ public class QuestionController {
         content.setActions(okButton);
 
         JFXDialog  dialog = new JFXDialog(mainStack, content, JFXDialog.DialogTransition.CENTER);
+        dialog.setOverlayClose(false);
 
         okButton.setOnAction(e -> {
             if(pField.getText().equals("zacker")) {
                 try {
-                    BufferedReader reader = new BufferedReader(new FileReader("data/" + username + ".codart"));
-                    String username = reader.readLine();
-                    int[] userScoreInfo = new int[9];
-                    for(int i = 0; i < 9; i++)
-                        userScoreInfo[i] = Integer.parseInt(reader.readLine());
-                    reader.close();
+                    if(callId == 1) {
+                        BufferedReader reader = new BufferedReader(new FileReader("data/" + username + ".codart"));
+                        String username = reader.readLine();
+                        int[] userScoreInfo = new int[9];
+                        for (int i = 0; i < 9; i++)
+                            userScoreInfo[i] = Integer.parseInt(reader.readLine());
+                        reader.close();
 
-                    userScoreInfo[0] += 50;
-                    userScoreInfo[1] += 1;
-                    if(difficulty.equals("e"))
-                        userScoreInfo[2] += 1;
-                    else if(difficulty.equals("m"))
-                        userScoreInfo[3] += 1;
-                    else
-                        userScoreInfo[4] += 1;
+                        userScoreInfo[5] += 1;
+                        if (difficulty.equals("e")) {
+                            userScoreInfo[0] -= 20;
+                            userScoreInfo[6] += 1;
+                        } else if (difficulty.equals("m")) {
+                            userScoreInfo[0] -= 15;
+                            userScoreInfo[7] += 1;
+                        } else {
+                            userScoreInfo[0] -= 10;
+                            userScoreInfo[8] += 1;
+                        }
 
-                    writeToUserFile(username, userScoreInfo);
+                        writeToUserFile(username, userScoreInfo);
+                        writeToUserQuestionData();
 
-                    String questions[] = readQuestionsFile();
-                    String questionData[] = questions[index].split(" ");
-                    questionData[2] = "S";
-                    questions[index] = String.join(" ", questionData);
-                    writeToQuestionFile(username, questions);
-                    writeToUserQuestionData();
+                        String questions[] = readQuestionsFile();
+                        String questionData[] = questions[index].split(" ");
+                        questionData[2] = "F";
+                        questions[index] = String.join(" ", questionData);
+                        writeToQuestionFile(username, questions);
+                    } else {
+                        BufferedReader reader = new BufferedReader(new FileReader("data/" + username + ".codart"));
+                        String username = reader.readLine();
+                        int[] userScoreInfo = new int[9];
+                        for (int i = 0; i < 9; i++)
+                            userScoreInfo[i] = Integer.parseInt(reader.readLine());
+                        reader.close();
 
-                    String questions2[] = readQuestionsFile();
-                    String questionData2[] = questions[index].split(" ");
-                    questionData2[2] = "F";
-                    questions2[index] = String.join(" ", questionData);
-                    writeToQuestionFile(username, questions);
+                        userScoreInfo[0] += 50;
+                        userScoreInfo[1] += 1;
+                        if (difficulty.equals("e"))
+                            userScoreInfo[2] += 1;
+                        else if (difficulty.equals("m"))
+                            userScoreInfo[3] += 1;
+                        else
+                            userScoreInfo[4] += 1;
+
+                        writeToUserFile(username, userScoreInfo);
+                        writeToUserQuestionData();
+
+                        String questions[] = readQuestionsFile();
+                        String questionData[] = questions[index].split(" ");
+                        questionData[2] = "S";
+                        questions[index] = String.join(" ", questionData);
+                        writeToQuestionFile(username, questions);
+                    }
+
+                    String newQuestionData = selected + " " + (found + 1) + " NS " + System.nanoTime();
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_questions.codart", true));
+                    writer.write(newQuestionData);
+                    writer.newLine();
+                    writer.close();
                     dialog.close();
-
-                    addNewQuestion();
+                    loadTabs();
                 } catch (Exception e1) {
                     e1.printStackTrace();
                 }
@@ -299,15 +276,6 @@ public class QuestionController {
 
         dialog.setOverlayClose(false);
         dialog.show();
-    }
-
-    public void writeToQuestionFile(String username, String[] questionsData) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_questions.codart"));
-        for(String questionData: questionsData) {
-            writer.write(questionData);
-            writer.newLine();
-        }
-        writer.close();
     }
 
     public String[] readQuestionsFile() throws Exception {
@@ -319,6 +287,38 @@ public class QuestionController {
         reader.close();
         String questionsInfoArray[] =  questionsInfo.toArray(new String[questionsInfo.size()]);
         return  questionsInfoArray;
+    }
+
+    public void writeToQuestionFile(String username, String[] questionsData) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_questions.codart"));
+        for(String questionData: questionsData) {
+            writer.write(questionData);
+            writer.newLine();
+        }
+        writer.close();
+    }
+
+    public void writeToUserQuestionData() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("data/" + username + "_qdata.codart"));
+        String solvedState[][] = new String[3][];
+        solvedState[0] = reader.readLine().split(" ");
+        solvedState[1] = reader.readLine().split(" ");
+        solvedState[2] = reader.readLine().split(" ");
+        reader.close();
+        if(difficulty.equals("e")) {
+            solvedState[0][qNo - 1] = "T";
+        } else if(difficulty.equals(("m"))) {
+            solvedState[1][qNo - 1] = "T";
+        } else {
+            solvedState[2][qNo - 1] = "T";
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("data/" + username + "_qdata.codart"));
+        for(int i = 0; i < 3; i++) {
+            writer.write(String.join(" ", solvedState[i]));
+            writer.newLine();
+        }
+        writer.close();
     }
 
     public void writeToUserFile(String username, int[] userScoreInfo) throws IOException {
@@ -352,10 +352,11 @@ public class QuestionController {
     public void initVariables(String username, String[] questionData, int questionIndex) throws Exception{
         questionCard.depthProperty().set(1);
         questionForfeited.setDisable(true);
-        this.username = username;
-        no = Integer.parseInt(questionData[1]);
+        qNo = Integer.parseInt(questionData[1]);
         index = questionIndex;
         startTime = Long.parseLong(questionData[3]);
+        this.username = username;
+
         timerThread = new Thread(() -> {
                while(!exit) {
                    Long seconds = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
@@ -369,7 +370,7 @@ public class QuestionController {
                        exit = true;
                    } else {
                        int secondsLeft = (int) (30 - seconds);
-                       String minutesLeft = ((int)(secondsLeft / 60)) + "";
+                       String minutesLeft = (secondsLeft / 60) + "";
                        String remainderSeconds = (secondsLeft - (Integer.parseInt(minutesLeft) * 60)) + "";
                        if(minutesLeft.length() == 1)
                            minutesLeft = "0" + minutesLeft;
@@ -383,6 +384,7 @@ public class QuestionController {
                                }
                        );
                    }
+
                    try {
                        Thread.sleep(1000);
                    } catch (InterruptedException e) {
@@ -399,24 +401,27 @@ public class QuestionController {
             questionTimer.setText("    :    ");
         }
 
-        String diff = questionData[0];
-        difficulty = diff;
-        int qNo = Integer.parseInt(questionData[1]), counter = 1;
+        difficulty = questionData[0];
+
+        int counter = 1;
         String line, title = "", body = "";
         boolean titleEncountered = false;
-        BufferedReader reader = new BufferedReader(new FileReader("data/" + diff + ".codart"));
+        BufferedReader reader = new BufferedReader(new FileReader("data/" + difficulty + ".codart"));
         while ((line = reader.readLine()) != null) {
             if(qNo == counter && titleEncountered) {
                 int minNoOfLines = 26;
                 body += line + "\n";
+
                 while (!(line = reader.readLine()).equals("-x-x-x-")) {
                     body += line + "\n";
                     minNoOfLines--;
                 }
+
                 while (minNoOfLines >= 0) {
                     body += "\n";
                     minNoOfLines--;
                 }
+
                 reader.close();
                 break;
             } else if (qNo == counter){
@@ -428,16 +433,14 @@ public class QuestionController {
                 counter++;
         }
 
-        if(diff.equals("e"))
-            diff = "EASY";
-        else if(diff.equals("m"))
-            diff = "MEDIUM";
+        if(difficulty.equals("e"))
+            questionType.setText("QUESTION: EASY");
+        else if(difficulty.equals("m"))
+            questionType.setText("QUESTION: MEDIUM");
         else
-            diff = "HARD";
+            questionType.setText("QUESTION: HARD");
 
-        questionTitle.setText("QUESTION: " + diff);
         questionTitle.setText(title);
         questionBody.setText(body);
     }
 }
-
